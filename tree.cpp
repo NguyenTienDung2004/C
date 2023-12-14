21d43e5b72f942f4bbca90278986f9a665a29de1
NguyenTienDung2004 <dung.220521@student.hactech.edu.vn>
2023-12-14 19:26:01 +0700

    them file tree.cpp

---
+++ tree.cpp
@@ -0,0 +1,19 @@
+#include"tree.h"
+int main(){
+//	node tr;
+//	create_empty_tree(tr);
+//	insert_node(tr, 1);
+//	insert_node(tr, 2);
+//	insert_node(tr, 3);
+//	insert_node(tr, 4);
+//	insert_node(tr, 5);
+//	insert_node(tr, 6);
+//	insert_node(tr, 7);
+//	insert_node(tr, -1);
+//	delete_any_node(tr, 1);
+//	NLR(tr);
+
+node basic_tr;
+create_tree_basic(basic_tr);
+NLR(basic_tr);
+}
